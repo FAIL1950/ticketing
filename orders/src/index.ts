030d3d8f2fafbeff21fb6cc40500c8aea953d85a
FAIL1950 <fail19505@gmail.com>
2025-10-24 04:58:48 +0300

    added startup message to orders service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -25,6 +25,8 @@ if (!process.env.NATS_STREAM_NAME_FOR_PUBLISH) {
 
 let server: Server;
 
+console.log('Starting...')
+
 const handleShutdown = async (signal: string) => {
     console.log(`Received ${signal}, starting graceful shutdown...`);
 
