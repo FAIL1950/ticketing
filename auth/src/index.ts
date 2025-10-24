2c4b64ae3d39535292cf93a83bb897f19017d2ec
FAIL1950 <fail19505@gmail.com>
2025-10-24 04:15:17 +0300

    added startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -9,6 +9,8 @@ if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
 }
 
+console.log('Starting up...');
+
 try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
